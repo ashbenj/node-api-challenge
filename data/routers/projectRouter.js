49a737b15a8cd05d199d208bec45d75bcc055329
Asha <ashalbenjamin@gmail.com>
2020-05-17 20:58:17 -0500

    actionRouter complete

---
+++ data/routers/projectRouter.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const Projects = require('../helpers/projectModel');
+const router = express.Router();
+
+router.get('/:id', (req, res) => {});
+
+router.post('/', (req, res) => {});
+
+router.put('/:id', (req, res) => {});
+
+router.delete('/:id', (req, res) => {});
+
+router.get('/:id', (req, res) => {});
+
+module.exports = router;
